97e16320c8e48a94c7cdf18efc1aecd62bcec515
Adnanalislam <adnanalislam227@gmail.com>
2018-04-30 10:57:45 +0600

    waiter interface added

---
+++ src/takeout/Waiter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package takeout;
+
+/**
+ *
+ * @author asif
+ */
+public interface Waiter {
+    public void addPatty();
+    public void addSauce();
+    public void addCheese();
+    public Burger getBurger();
+}
