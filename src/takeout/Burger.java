e3464dfd94c4910c826d78d8778a7251deb26245
Adnanalislam <adnanalislam227@gmail.com>
2018-04-30 10:55:36 +0600

    Burger class added

---
+++ src/takeout/Burger.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package takeout;
+
+/**
+ *
+ * @author asif
+ */
+public class Burger {
+    private String patty;
+    private String sauce;
+    private String cheese;
+
+    public void setPatty(String patty) {
+        this.patty = patty;
+    }
+
+    public void setSauce(String sauce) {
+        this.sauce = sauce;
+    }
+
+    public void setCheese(String cheese) {
+        this.cheese = cheese;
+    }
+
+    public String getPatty() {
+        return patty;
+    }
+
+    public String getSauce() {
+        return sauce;
+    }
+
+    public String getCheese() {
+        return cheese;
+    }
+    
+    public void show(){
+        
+    }
+    
+}
